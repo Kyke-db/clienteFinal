c3b4ef2c8e4c03f5b3cd8599c8ace51ba7b8a8ef
Kyke <contactokyke@gmail.com>
2025-05-23 10:00:36 -0400

    Initial commit

---
+++ src/main/java/com/digipymes360/clienteFinal/ClienteFinalApplication.java
@@ -0,0 +1,13 @@
+package com.digipymes360.clienteFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digipymes360/clienteFinal/ClienteFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digipymes360.clienteFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
